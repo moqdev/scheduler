6247f535ad0848a5e3ba11325043a679aeebae06
Mona Waseem <monaqwaseem@gmail.com>
2021-06-02 19:11:15 +0000

    Connect snake game to the server.

---
+++ w2/d3/play.js
@@ -0,0 +1,20 @@
+// establishes a connection with the game server
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542,
+  });
+
+  // interpret incoming data as text
+  //
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+
+
+console.log("Connecting ...");
+connect();
