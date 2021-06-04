f731585a8db1d9f1b11ccacc3e5a3721b2d17e03
Mona Waseem <monaqwaseem@gmail.com>
2021-06-04 12:19:16 +0000

    Built logic to catch specific cat breed details.

--- w2/d4-api/breedFetcher.js
+++ w2/d4-api/breedFetcher.js
@@ -1,13 +1,19 @@
 const request = require("request")
-let breedName = process.argv[2].substring(0, 4) || "sibe";
-request('https://api.thecatapi.com/v1/images/search?breed_ids=' + breedName, (error, response, body) => {
-  if (error) {
-    return (error);
-  }
-  // console.log(typeof body)
-  // console.log(response.body);
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  console.log(data[0].breeds[0].description);
-})
+// let breedName = process.argv[2].substring(0, 4) || "sibe";
+// request('https://api.thecatapi.com/v1/images/search?breed_ids=' + breedName, (error, response, body) => {
+//   if (error) {
+//     return (error);
+//   }
+//   // console.log(typeof body)
+//   // console.log(response.body);
+//   const data = JSON.parse(body);
+//   // console.log(data);
+//   // console.log(typeof data);
+//   console.log(data[0].breeds[0].description);
+// })
+const example = 'example string';
+console.log(example.length); //out 14
+
+//accessing-array-values.js
+let food = ['apple', 'pizza', 'pear']
+console.log(food[1]);
